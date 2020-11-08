e4a29adff8467c396ab2c209274b4adf6b640083
Idrissa KAMARA <idykamara@gmail.com>
2020-11-08 13:49:08 +0000

    modification

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
 #this is my first program in python 
+
+
+echo "Hello world!"
+
+import pandas as pd 
+df=pd.read_csv("sih_data.csv")
+
+df.head()
+
+df.tail()
+
+test 
+
+je veux ajouter ce test et voir 
